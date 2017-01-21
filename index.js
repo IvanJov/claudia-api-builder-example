123687d4ee9b25d4cf50a340cddd618f3860f0d1
Ivan Jovanovic <ivanjov96@gmail.com>
2017-01-21 20:25:58 +0100

    Example API

---
+++ index.js
@@ -0,0 +1,50 @@
+/*global require, module*/
+var ApiBuilder = require('claudia-api-builder'),
+  api = new ApiBuilder(),
+  fs = require('fs'),
+  superb = require('superb'),
+  denodeify = require('denodeify');
+
+module.exports = api;
+
+// just return the result value for synchronous processing
+api.get('/hello', function () {
+  'use strict';
+  return 'hello world';
+});
+
+// pass some arguments using the query string or headers to this
+// method and see that they're all in the request object
+api.get('/echo', function (request) {
+  'use strict';
+  return request;
+});
+
+// use request.queryString for query arguments
+api.get('/greet', function (request) {
+  'use strict';
+  return request.queryString.name + ' is ' + superb();
+});
+
+// use {} for dynamic path parameters
+api.get('/people/{name}', function (request) {
+  'use strict';
+  return request.pathParams.name + ' is ' + superb();
+});
+
+// Return a promise for async processing
+api.get('/packagejson', function () {
+  'use strict';
+  var read = denodeify(fs.readFile);
+  return read('./package.json')
+    .then(JSON.parse)
+    .then(function (val) {
+      return val;
+    });
+});
+
+// use .post to handle a post; or .delete, .patch, .head, .put
+api.post('/echo', function (request) {
+  'use strict';
+  return request;
+});
